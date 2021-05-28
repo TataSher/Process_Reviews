16dec003fc3cd4c8fa68cb7cba27911338bc357d
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-28 12:35:11 +0100

    Test for band_pass_filter receiving 3 arguments passes

--- Process_review_2/lib/band_pass_filter.rb
+++ Process_review_2/lib/band_pass_filter.rb
@@ -1,4 +1,4 @@
 
-def band_pass_filter(frequencies, lower_limit = 40)
+def band_pass_filter(frequencies, lower_limit = 40, upper_limit=1000)
   @frequencies = frequencies
 end
--- Process_review_2/spec/band_pass_filter_spec.rb
+++ Process_review_2/spec/band_pass_filter_spec.rb
@@ -8,6 +8,10 @@ describe 'band pass filter' do
   it 'receives an array of frequencies, lower limit and returns an array' do
     expect(band_pass_filter([500], 30)).to eq([500])
   end
+
+  it 'receives an array of frequencies,lower limit and upper limit and returns an array' do
+    expect(band_pass_filter([500], 30, 900)).to eq([500])
+  end
 end
 
 
