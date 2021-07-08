8a1a332c0e492f35a14a3f99fd9232c4668fa226
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-08 15:38:15 +0100

    Refactored - omitted new array and used map method fully. Also renamed sentence for clarity

--- Process_review_3/lib/spell_checker.rb
+++ Process_review_3/lib/spell_checker.rb
@@ -1,11 +1,10 @@
 
-def spell_checker(string)
-  raise 'Input is not a string' if !string.is_a? String 
-  
+def spell_checker(sentence)
+  raise 'Input is not a string' if !sentence.is_a? String 
+
   correct_spellings = ["string", "and", "number"]
-  result = []
-  string.split.map do |word|
-    correct_spellings.include?(word.downcase) ? result << word : result << "~#{word}~"
+  checked_sentence = sentence.split.map do |word|
+    correct_spellings.include?(word.downcase) ? word : "~#{word}~"
   end
-  result.join(" ")
+  checked_sentence.join(" ")
 end
