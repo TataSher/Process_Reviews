fbe4c4f00e3d82628dc051e5f8e194f54c9fa073
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-08 14:38:11 +0100

    solution refactored

--- Process_review_3/lib/spell_checker.rb
+++ Process_review_3/lib/spell_checker.rb
@@ -3,11 +3,7 @@ def spell_checker(string)
   correct_spellings = ["String", "and", "Number"]
   result = []
   string.split.map do |word|
-    if correct_spellings.include?(word)
-      result << word
-    else
-      result << "~#{word}~"
-    end
+    correct_spellings.include?(word) ? result << word : result << "~#{word}~"
   end
   result.join(" ")
 end
