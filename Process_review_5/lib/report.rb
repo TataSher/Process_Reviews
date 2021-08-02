d2d1aca9d3617978f25d66febbf0e7f4f01ee2ff
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-02 16:09:20 +0100

    add result adds single values of green, amber and red

--- Process_review_5/lib/report.rb
+++ Process_review_5/lib/report.rb
@@ -3,15 +3,21 @@ class Report
 
   def initialize
     @green = 0
+    @amber = 0
+    @red = 0
   end
 
   def add_result(test_result)
     if test_result === 'Green' 
       @green = @green + 1
+    elsif test_result === 'Amber'
+      @amber = @amber + 1
+    elsif test_result === 'Red'
+      @red = @red + 1
     end
   end
   
   def print_report 
-    "Test 1:\nGreen: #{@green}\nAmber: 0\nRed: 0"
+    "Test 1:\nGreen: #{@green}\nAmber: #{@amber}\nRed: #{@red}"
   end
 end
