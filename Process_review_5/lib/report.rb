91947f0e27f7d344cf5c205c8253566e477208d8
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-02 16:03:41 +0100

    Rewrote the green test in a stateful manner

--- Process_review_5/lib/report.rb
+++ Process_review_5/lib/report.rb
@@ -2,10 +2,16 @@ class Report
   attr_reader :log
 
   def initialize
-    @log = []
+    @green = 0
   end
 
   def add_result(test_result)
-   @log << test_result.split(', ')
+    if test_result === 'Green' 
+      @green = @green + 1
+    end
+  end
+  
+  def print_report 
+    "Test 1:\nGreen: #{@green}\nAmber: 0\nRed: 0"
   end
 end
--- Process_review_5/spec/report_spec.rb
+++ Process_review_5/spec/report_spec.rb
@@ -5,17 +5,7 @@ describe Report do
   describe '#add_result' do
     it 'add test results to report' do
       report.add_result('Green')
-      expect(report.log).to eq([['Green']])
-    end
-    it 'adds multiple values from a string to a log' do
-      report.add_result('Green, Red')
-      expect(report.log).to eq([['Green', 'Red']])
-    end
-    it 'adds values to the log in sets' do
-      report.add_result('Green, Red')
-      report.add_result('Amber')
-
-      expect(report.log).to eq([['Green', 'Red'], ['Amber']])
+      expect(report.print_report).to eq("Test 1:\nGreen: 1\nAmber: 0\nRed: 0")
     end
   end
 end
